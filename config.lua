2ad4b4c92ecb28091486620923252e5df2ee2f2f
L0Wigh <mathiotthomas@gmail.com>
2021-10-15 22:19:14 +0200

    Fixing nvim-tree keybinds

--- config.lua
+++ config.lua
@@ -252,15 +252,14 @@ lvim.builtin.which_key.mappings = {
 
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
--- default mappings
 local newMap = {
-	{ key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+	{ key = {"<CR>"}, cb = tree_cb("edit") },
 	{ key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
 	{ key = "<C-v>",                        cb = tree_cb("vsplit") },
 	{ key = "<C-x>",                        cb = tree_cb("split") },
 	{ key = "<C-t>",                        cb = tree_cb("tabnew") },
-	{ key = "l",                            cb = tree_cb("prev_sibling") },
-	{ key = "k",                            cb = tree_cb("next_sibling") },
+	{ key = "<",                            cb = tree_cb("prev_sibling") },
+	{ key = ">",                            cb = tree_cb("next_sibling") },
 	{ key = "P",                            cb = tree_cb("parent_node") },
 	{ key = "<BS>",                         cb = tree_cb("close_node") },
 	{ key = "<S-CR>",                       cb = tree_cb("close_node") },
@@ -270,7 +269,7 @@ local newMap = {
 	{ key = "I",                            cb = tree_cb("toggle_ignored") },
 	{ key = "H",                            cb = tree_cb("toggle_dotfiles") },
 	{ key = "R",                            cb = tree_cb("refresh") },
-	{ key = "a",                            cb = tree_cb("create") },
+	{ key = "n",                            cb = tree_cb("create") },
 	{ key = "d",                            cb = tree_cb("remove") },
 	{ key = "r",                            cb = tree_cb("rename") },
 	{ key = "<C-r>",                        cb = tree_cb("full_rename") },
