48a8d4acecf4a652842ed77b239ca31489df9c18
L0Wigh <mathiotthomas@gmail.com>
2021-10-15 05:00:59 +0200

    version 1.1

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
 LVBRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
+echo "\n\n\n"
+echo "----------------"
+echo "Installing SolarVim"
 mv ~/.local/share/lunarvim/lvim/lua/lvim/core/dashboard.lua ~/.local/share/lunarvim/lvim/lua/lvim/core/dashboard.lua.bak
 cp ./dashboard.lua ~/.local/share/lunarvim/lvim/lua/lvim/core/dashboard.lua
 mv ~/.config/lvim/config.lua ~/.config/lvim/config.lua.bak
 cp ./config.lua  ~/.config/lvim/config.lua
+mv ~/.local/bin/lvim ~/.local/bin/svim
+echo "Now launch svim and use :PackerUpdate"
+echo "or use <space>ppu to install additional plugins"
