390ccacef90ce012a739704af30e9493296f22a1
L0Wigh <mathiotthomas@gmail.com>
2021-10-15 04:42:37 +0200

    version 1.0

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 LVBRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
 mv ~/.local/share/lunarvim/lvim/lua/lvim/core/dashboard.lua ~/.local/share/lunarvim/lvim/lua/lvim/core/dashboard.lua.bak
-# curl dashboard or mv
+cp ./dashboard.lua ~/.local/share/lunarvim/lvim/lua/lvim/core/dashboard.lua
 mv ~/.config/lvim/config.lua ~/.config/lvim/config.lua.bak
-# curl config.lua or mv
+cp ./config.lua  ~/.config/lvim/config.lua
